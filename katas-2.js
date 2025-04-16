e48a71b392507248018b847a6e9da190f0c70077
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2025-04-16 07:22:46 -0400

    Update katas-2.js

--- katas-2.js
+++ katas-2.js
@@ -1,4 +1,54 @@
+/* RANDOM STUDENT KATA:  */
 
+// Who will be gatekeeper and keymaster today?
+
+/* Global Variables */
+// Create an array called students, start empty. 
+// Create an array called picked, start empty. 
+
+
+/* Function Main()
+ * Set variable called count to asking how many students there are.
+ * Fill global array students by passing count as an argument to addStudents
+ * Set gateKeeper = randStudent(), the name of a random student in the list
+ * Set keymaster = randNewStudent(), name of a different random student
+ * Display gatekeeper name and keymaster name for this kata
+ * @param = none
+ * @return = none
+ */
+
+
+/* Function addStudents(count) 
+ * Use a for loop to add student names to the students array via user input.
+ * The loop should run count number of times. 
+ * @param = count {integer}
+ * @return = none
+ */
+
+
+/* Function randStudent()
+ * Set student = random number, 0 to length of students array.
+ * Set name = element in students array at that random index.
+ * Add name to picked array. 
+ * Return the name of the random student
+ * @param = none
+ * @return = name {string}
+ */
+
+/* Function randNewStudent() (start with copy of randStudent)
+ * Set student = "" and picked = 0. 
+ * Loop while picked is not equal to -1
+ * Set student = random number, 0 to length of students array.
+ * Set name = element in students array at that random index.
+ * Set picked = the index of that name the picked array. 
+ * End the while loop.
+ * Add name to picked array.
+ * Return the name of the new random student
+ * @param = none
+ * @return = name {string}
+ */
+
+ 
 /* ************************************* */
 
 
